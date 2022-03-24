13a08ff6439ae1c447f3c638a46f5f421e80eb16
Edgar Avila Gonzalez <avilaedgar2001@gmail.com>
2022-03-23 19:45:36 -0600

    pÂrimer commit

---
+++ launchx-vuex/src/components/botones.vue
@@ -0,0 +1,15 @@
+<template>
+     <div class="buttons">
+      <button @click="$store.commit('bajarContador')">-</button>
+      <button @click="$store.commit('subirContador')">+</button>
+      <button @click="$store.dispatch('subirContadorRandom')">Subir Contador Random</button>
+      <button @click="$store.dispatch('bajarContadorRandom')">Bajar Contador Random</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Botones',
+    
+}
+</script>
